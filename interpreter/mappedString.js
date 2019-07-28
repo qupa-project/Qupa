30d6a3502cae940ce17da465fae898dff347c1ee
Ajani Bilby <aj@bilby.net>
2019-07-28 21:26:20 +1000

    +Class MappedString
    MappedString allows a string to keep it's column, and line references while being manipulated.

---
+++ interpreter/mappedString.js
@@ -0,0 +1,97 @@
+class CharData{
+	constructor (char, column, line, file){
+		this.char   = char;
+		this.column = column;
+		this.line   = line;
+		this.file   = file;
+	}
+
+	toString(){
+		return `(${this.column}:${this.line})`;
+	}
+}
+
+
+/**
+ * Allows for string manipulation while maintaining character column and line location references
+ */
+class MappedString{
+	constructor(string, filename="Unknown File"){
+		this.filename = filename;
+		this.data = [];
+
+		let column = 1;
+		let line = 1;
+		for (let char of string){
+			this.data.push(new CharData(char, column, line, filename))
+
+			// Move mapping position one line
+			if (char == "\n"){
+				column = 0;
+				line++;
+			}
+			column++;
+		}
+	}
+
+	/**
+	 * Return the number of characters in the string
+	 * @returns {Number}
+	 */
+	get length(){
+		return this.data.length;
+	}
+
+	/**
+	 * Return a specific character at a given index
+	 * @param {Number} index
+	 * @returns {String}
+	 */
+	get(index){
+		return this.data[index].char;
+	}
+
+	/**
+	 * Return an address (file, column, line) of a specific character index
+	 * @param {Number} index
+	 * @returns {String}
+	 */
+	getAddress(index){
+		return `"${this.filename}" `+this.data[index].toString();
+	}
+
+	/**
+	 * Returns the contents of the mapped string as a normal string
+	 * @returns {String}
+	 */
+	toString(){
+		let output = "";
+
+		for (let i=0; i<this.data.length; i++){
+			output += this.data[i].char;
+		}
+
+		return output;
+	}
+
+
+	/**
+	 * Slice the mapped string - behaves as normal string slice
+	 * @param {Number} start
+	 * @param {Number} end
+	 * @returns {MappedString}
+	 */
+	slice(start=0, end=NaN){
+		if (isNaN(end)){
+			end = this.length;
+		}
+
+		let result = new MappedString("", this.filename);
+		result.data = this.data.slice(start, end);
+
+		return result;
+	}
+}
+
+
+module.exports = MappedString;
