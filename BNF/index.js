25c57f976278483021fd06205c5b329cae69efbd
Ajani Bilby <me@ajanibilby.com>
2020-05-04 19:14:04 +1000

    ~Updated test for new compilation tests

--- BNF/index.js
+++ BNF/index.js
@@ -5,10 +5,15 @@ let tree = JSON.parse(fs.readFileSync('./bnf.json', 'utf8'));
 let Compile = require('./compiler.js');
 let Parse = require('./parser.js');
 
-// data = `"\\"" ( "\\"" | "\\n" | "\\\\" | "\\t" | s!( "\\"" | "\\\\" ) )+ "\\""`
 let res = Parse(data, tree, "program");
 fs.writeFileSync('temp-tree.json', JSON.stringify(res, null, 2));
 
+if (res.hasError || res.isPartial) {
+	console.error("BNF didn't parse correctly");
+	process.exit(1);
+}
+// process.exit(0);
+
 
 let syntax = Compile(res.tree);
-fs.writeFileSync('temp-syntax.json', JSON.stringify(syntax, null, 2));
+fs.writeFileSync('bnf.json', JSON.stringify(syntax, null, 2));
