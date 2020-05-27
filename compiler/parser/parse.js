759c1489d7200a105ac7c04145e414b425b1e02a
Ajani Bilby <me@ajanibilby.com>
2020-05-27 17:07:51 +1000

    ~Updated syntax error messages to be more concise

--- compiler/parser/parse.js
+++ compiler/parser/parse.js
@@ -8,25 +8,21 @@ const syntax = BNF.types.BNF_Tree.fromJSON(
 
 function Parse (data, filename){
   // Parse the file and check for errors
-  let tree;
-  try {
-    tree = BNF.Parse(data, syntax, "program");
-  } catch(e) {
-    throw new Error(`An internal error occured when attempting to parse the data;\n  ${e}`)
-  }
+  let tree = BNF.Parse(data, syntax, "program");
 
   if (tree.hasError || tree.isPartial) {	
     let ref = null;
     if (tree.tree instanceof BNF.types.BNF_SyntaxError) {
       ref = tree.tree.ref;
     } else {
-      ref = tree.tree.ref.end;
+      ref = tree.tree.ref.reached || tree.tree.ref.end;
     }
 
     let msg = filename ? `${filename}: ` : "";
-    msg += `BNF did not parse correctly due to a syntax error at ${ref.toString()}\n`;
+    msg += `Syntax error at ${ref.toString()}\n`;
     msg += "  " + BNF.Message.HighlightArea(data, ref).split('\n').join('\n  ');
-    throw new SyntaxError(msg);
+    console.error(msg);
+    process.exit(1);
 	}
 	
 	// TODO: Simplfy tree output
