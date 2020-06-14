91c3bfbff902818d8b474de2b070825132ef5c1a
Ajani Bilby <me@ajanibilby.com>
2020-06-14 14:28:37 +1000

    Compile Scripts

---
+++ compiler/compile.js
@@ -0,0 +1,18 @@
+const Project = require('./component/project');
+
+const path = require('path');
+
+const version = "0.0.0";
+const root = path.resolve("./");
+
+if (process.argv.indexOf("-v") != -1) {
+	console.log(version);
+	process.exit(0);
+}
+
+let origin = path.resolve(root, process.argv[2]);
+
+let project = new Project(root);
+project.import(origin);
+
+console.log(project);
