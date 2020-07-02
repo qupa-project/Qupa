6e725358a88bc4c0e71bcdb91df6eab259801315
Ajani Bilby <me@ajanibilby.com>
2020-07-03 00:26:52 +1000

    +Single command compilation

--- compiler/compile.js
+++ compiler/compile.js
@@ -1,7 +1,9 @@
 const Project = require('./component/project');
 
 const path = require('path');
+const os = require('os');
 const fs = require('fs');
+const { exec } = require('child_process');
 
 const version = "0.0.0";
 const root = path.resolve("./");
@@ -11,7 +13,17 @@ if (process.argv.indexOf("-v") != -1) {
 	process.exit(0);
 }
 
+let index = process.argv.indexOf('-o');
+let output = "out.ll";
+if (index != -1 && index > 2) {
+	output = process.argv[index+1];
+}
+
 let origin = path.resolve(root, process.argv[2]);
+if (!fs.existsSync(origin)) {
+	console.error(`Invalid file name: ${origin}`);
+	process.exit(1);
+}
 
 let project = new Project(root);
 project.import(origin);
@@ -23,4 +35,24 @@ if (project.error) {
 }
 let asm = project.compile();
 
-fs.writeFileSync('out.ll', asm.toLLVM(), 'utf8');
+fs.writeFileSync(output, asm.toLLVM(), 'utf8');
+
+if (process.argv.indexOf('--source') == -1) {
+	console.info("Compiling to executable...");
+	let runtime_path = path.resolve(__dirname, "./../runtime/runtime.ll");
+
+	let exec_out = output.slice(0, output.indexOf("."));
+	if (os.platform() == "win32") {
+		exec_out += ".exe";
+	} else if (os.platform() == "darwin") {
+		exec_out += ".app";
+	} else {
+		exec_out += ".o";
+	}
+
+	exec(`clang++ ${runtime_path} ${output} -o ${exec_out}`, (err) => {
+		if (err) {
+			console.error(err);
+		}
+	})
+}
